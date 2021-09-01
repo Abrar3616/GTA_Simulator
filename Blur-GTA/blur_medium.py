45087daf46de7492a6fffa50b642f24c97e16aac
Abrar3616 <74458556+Abrar3616@users.noreply.github.com>
2021-09-01 14:47:38 +0500

    Update blur_medium.py

--- Blur-GTA/blur_medium.py
+++ Blur-GTA/blur_medium.py
@@ -5,8 +5,7 @@ import numpy as np
 import cv2 as cv
 from time import sleep
 
-#dir = "/home/abrar/gta_live/images/"
-dir = "/home/abrar/trajectory-6/"
+dir = "/home/abrar/gta_live/images/"
 
 i = 2
 h = 480
@@ -21,13 +20,13 @@ while(True):
             break
     im2 = cv.imread(dir + "{:04d}".format(i-1) + ".jpg")
     im3 = cv.imread(dir + "{:04d}".format(i-2) + ".jpg")
-    #im1r = cv.resize(im1, (640, 480), interpolation = cv.INTER_AREA)
-    #im2r = cv.resize(im2, (640, 480), interpolation = cv.INTER_AREA)
-    #im3r = cv.resize(im3, (640, 480), interpolation = cv.INTER_AREA)
+    im1r = cv.resize(im1, (640, 480), interpolation = cv.INTER_AREA)
+    im2r = cv.resize(im2, (640, 480), interpolation = cv.INTER_AREA)
+    im3r = cv.resize(im3, (640, 480), interpolation = cv.INTER_AREA)
     imarr1 = np.array(im1,dtype=np.float)
     imarr2 = np.array(im2,dtype=np.float)
     imarr3 = np.array(im3,dtype=np.float)
     arr = imarr1/3 + imarr2/3 + imarr3/3
     arr = np.array(np.round(arr),dtype=np.uint8)
-    cv.imwrite("/home/abrar/trajectory-6-blur/medium1_{:04d}.jpg".format(i), arr)
+    cv.imwrite("/home/abrar/blur_gta/medium_{:04d}.jpg".format(i), arr)
     i+=1
