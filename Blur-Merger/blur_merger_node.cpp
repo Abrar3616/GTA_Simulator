30bda94bb52faccac19d252db804dd1fb1e999cb
Abrar3616 <74458556+Abrar3616@users.noreply.github.com>
2021-09-01 14:57:32 +0500

    Update blur_merger_node.cpp

--- Blur-Merger/blur_merger_node.cpp
+++ Blur-Merger/blur_merger_node.cpp
@@ -14,27 +14,27 @@ int main(int argc, char* argv[])
     image_transport::ImageTransport it(nh);
     image_transport::Publisher            pub=it.advertise("/camera/image_raw",10);
     int i = 0;
-    stringstream ss;
-    std::string image_name;
-    ss << "/home/abrar/gta_blur.jpg";
-    image_name =  ss.str();
     cv::Mat image;
     sensor_msgs::ImagePtr msg;
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(15);
 
     while(nh.ok())
     {
-    	image = cv::imread(image_name,CV_LOAD_IMAGE_COLOR);
+        stringstream ss;
+        ss << "/home/abrar/gta_blur/" << std::to_string(i) << ".jpg";
+        std::string image_name;
+        image_name =  ss.str();
+        image = cv::imread(image_name,CV_LOAD_IMAGE_COLOR);
         if(!image.empty())
         {
             msg = cv_bridge::CvImage(std_msgs::Header(),"bgr8",image).toImageMsg();
             pub.publish(msg);
+            i++;
         }
-    	//ROS_INFO_STREAM(image_name);
+        //ROS_INFO_STREAM(image_name);
 
-    	ros::spinOnce();
-    	loop_rate.sleep();
-      i++;
+        ros::spinOnce();
+        loop_rate.sleep();
     }
 
 }
