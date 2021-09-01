3db07c88ba70f0fac65fc4b4bb806cc814cb7585
Abrar3616 <74458556+Abrar3616@users.noreply.github.com>
2021-09-01 15:03:03 +0500

    Update blur_high.py

--- Blur-GTA/blur_high.py
+++ Blur-GTA/blur_high.py
@@ -7,20 +7,20 @@ from time import sleep
 
 dir = "/home/abrar/gta_live/images/"
 
-i = 4
+i = 3
 h = 480
 w = 640
 arr = np.zeros((h,w,3),np.float)
 
 while(True):
-    im1 = cv.imread(dir + "{:04d}".format(i) + ".jpg")
+    im1 = cv.imread(dir + std::to_string(i) + ".jpg")
     if im1 is None:
         sleep(1)
         if im1 is None:
             break
-    im2 = cv.imread(dir + "{:04d}".format(i-1) + ".jpg")
-    im3 = cv.imread(dir + "{:04d}".format(i-2) + ".jpg")
-    im4 = cv.imread(dir + "{:04d}".format(i-3) + ".jpg")
+    im2 = cv.imread(dir + std::to_string(i-1) + ".jpg")
+    im3 = cv.imread(dir + std::to_string(i-2) + ".jpg")
+    im4 = cv.imread(dir + std::to_string(i-3) + ".jpg")
     im1r = cv.resize(im1, (640, 480), interpolation = cv.INTER_AREA)
     im2r = cv.resize(im2, (640, 480), interpolation = cv.INTER_AREA)
     im3r = cv.resize(im3, (640, 480), interpolation = cv.INTER_AREA)
@@ -31,5 +31,5 @@ while(True):
     imarr4 = np.array(im4,dtype=np.float)
     arr = imarr1/4 + imarr2/4 + imarr3/4 + imarr4/4
     arr = np.array(np.round(arr),dtype=np.uint8)
-    cv.imwrite("/home/abrar/gta_blur/high_{:04d}.jpg".format(i), arr)
+    cv.imwrite("/home/abrar/gta_blur/" + std::to_string(i) + ".jpg", arr)
     i+=1
