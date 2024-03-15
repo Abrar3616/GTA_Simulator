b1729350a69a115821e911cdfd4ed5ec8cb1aef4
Abrar Qureshi <abrar16qureshi@gmail.com>
2024-03-15 14:46:55 +0500

    Minor updates

--- Blur-GTA/blur_medium.py
+++ Blur-GTA/blur_medium.py
@@ -18,14 +18,18 @@ while(True):
         sleep(1)
         if im1 is None:
             break
+    
     im2 = cv.imread(dir + std::to_string(i-1) + ".jpg")
     im3 = cv.imread(dir + std::to_string(i-2) + ".jpg")
+
     im1r = cv.resize(im1, (640, 480), interpolation = cv.INTER_AREA)
     im2r = cv.resize(im2, (640, 480), interpolation = cv.INTER_AREA)
     im3r = cv.resize(im3, (640, 480), interpolation = cv.INTER_AREA)
+    
     imarr1 = np.array(im1,dtype=np.float)
     imarr2 = np.array(im2,dtype=np.float)
     imarr3 = np.array(im3,dtype=np.float)
+    
     arr = imarr1/3 + imarr2/3 + imarr3/3
     arr = np.array(np.round(arr),dtype=np.uint8)
     cv.imwrite("/home/abrar/gta_blur/" + std::to_string(i) + ".jpg", arr)
